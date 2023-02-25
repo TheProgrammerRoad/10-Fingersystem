95014cc92aaeda4390f80c429d836faf0c283c22
Yannik <arendt.yannik@gmail.com>
2023-02-25 21:13:31 +0100

    Jedes Zeichen wird jetzt in einem extra div dargestellt. Die Divs haben alle die gleiche Breite, so ist es später einfacher den Block nach links zu bewegen. Die nächsten Aufgaben sind denke ich offensichtlich. Außerdem wird die 'moveBlock' Methode nicht funktionieren. Das sollte ich wohl als nächstes machen, damit da kein sinnloser Code rumfliegt.

--- characters.js
+++ characters.js
@@ -9,37 +9,6 @@ function getCssStyle(element, prop) {
 }
 
 /**
- *
- * @param {*} eElement Get the width of the element in pixels
- * @returns The width of the element in pixels
- */
-function getCharacterWidth(sElement) {
-  const eCharacter = document.getElementById("currentCharacter");
-
-  const fontWeight = getCssStyle(eCharacter, "font-weight");
-  const fontSize = getCssStyle(eCharacter, "font-size");
-  const font = getCssStyle(eCharacter, "font");
-
-  var measurer = document.createElement("div");
-  measurer.style.visibility = "hidden";
-  measurer.style.height = "auto";
-  measurer.style.width = "auto";
-  measurer.style.position = "absolute";
-  measurer.style.whiteSpace = "pre";
-  measurer.innerHTML = sElement;
-  measurer.style.font = font;
-  measurer.style.fontSize = fontSize;
-  measurer.style.fontWeight = fontWeight;
-  document.body.appendChild(measurer);
-
-  const iCharacterWidth = Math.ceil(measurer.clientWidth);
-
-  document.body.removeChild(measurer);
-
-  return iCharacterWidth;
-}
-
-/**
  * Choses a random character from the characters array
  * @returns a random character from the characters array
  */
@@ -55,75 +24,22 @@ function getRandomCharacter() {
 }
 
 /**
- *
- * @returns An array of random characters
+ * Adds a <span> Element to the Characters Container
  */
-function buildCharacterBlock() {
-  let resultCharacterBlock = [];
+function addCharacterDivElementToCharactersContainer(eElement, sCharacter) {
+  const eCharacterDiv = document.createElement("div");
+  eCharacterDiv.innerHTML = sCharacter;
+  eCharacterDiv.classList.add('Character');
 
-  let tempCharacterBlock = [];
-  for (let i = 0; i < giCharacterBlockSizeMax; i++)
-    tempCharacterBlock.push(getRandomCharacter());
-
-  // change the last character to a new line
-  tempCharacterBlock[tempCharacterBlock.length - 1] = gsNewLineSpecialCharacter;
-
-  // The Enter symbol should be the last character.
-  // The user should then hit the enter key
-  // A new block of characters should be generated and displayed
-  let iCutIndex = tempCharacterBlock.indexOf(gsNewLineSpecialCharacter);
-  if (iCutIndex !== -1) tempCharacterBlock.splice(iCutIndex + 1);
-
-  // Group three characters together
-  let tempString = "";
-
-  for (let iIndex = 0; iIndex < tempCharacterBlock.length; iIndex++) {
-    if (iIndex % 3 === 0 && iIndex !== 0) {
-      resultCharacterBlock.push(tempString);
-      tempString = "";
-    }
-    tempString += tempCharacterBlock[iIndex];
-
-    if (iIndex === tempCharacterBlock.length - 1 && tempString.length > 0)
-      resultCharacterBlock.push(tempString);
-  }
-
-  return resultCharacterBlock;
+  eElement.appendChild(eCharacterDiv);
 }
 
 /**
  * Sets a new block of characters to be displayed on the screen
  */
-function setNewCharacterBlock() {
-  let eCharacter = document.getElementById("currentCharacter");
-  eCharacter.innerHTML = buildCharacterBlock().join(" ");
-
-  const sCharacter = eCharacter.innerHTML;
-  const iCharacterWidth = getCharacterWidth(
-    sCharacter.charAt(giKeyUpEventCounter)
-  );
-
-  // initially move the container half the character width plus half the letter-spacing width to the left
-  // so that current character is perfectly centered
-  const iPropertyLeft = window
-    .getComputedStyle(eCharacter)
-    .getPropertyValue("left");
-
-  const iHalfCharacterWidth = Math.ceil(iCharacterWidth / 2);
-  console.log("half-character width: " + iHalfCharacterWidth);
-
-  const iLetterSpacing =
-    parseInt(
-      window.getComputedStyle(eCharacter).getPropertyValue("letter-spacing")
-    ) || 0;
-  console.log("letter-spacing: " + iLetterSpacing);
-
-  eCharacter.style.left =
-    parseInt(iPropertyLeft) - iHalfCharacterWidth - iLetterSpacing / 2 + "px";
-
-  let iPropertyLeftNew = window
-    .getComputedStyle(eCharacter)
-    .getPropertyValue("left");
-
-  console.log("left: " + iPropertyLeftNew);
+function generateCharacterBlock() {
+  for (iCounter = 0; iCounter < giCharacterBlockSizeMax; iCounter++) {
+    const eCharacters = document.getElementById("charactersContainer");
+    addCharacterDivElementToCharactersContainer(eCharacters, getRandomCharacter());
+  }
 }
--- main.js
+++ main.js
@@ -1,22 +1,14 @@
 /** * Todo
  *
- * Version 11
- * 
- * Include transition: I think I need to use translateX instead of left = ...
- *
  * A Enter hit doesn't get recognized as correct. Also it starts with &. What happens currently if the current character is coincidently & too?
  *
  * There should at least three characters displayed not only the enter symbol or two more. At least three 'normal' characters
- *
- * Setup Git so that I have a file control system. With this I can make tags.
  *
  * Change Prettier settings. I don't like when the array gets formatted like this, see below
  *
  * Highlight the current character. Maybe give the current character a bigger font size
  *
  * Use different font-family, what is the difference?
- * 
- * Ich glaube beim Verschieben ist das '/ 2' ein Problem, da sich das häuft und dadurch zu weit nach rechts driftet
  *
  */
 
@@ -26,7 +18,7 @@
  */
 
 function main() {
-  setNewCharacterBlock();
+  generateCharacterBlock();
 }
 
 main();
